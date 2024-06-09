4997b4500368a1fd568e31864495359dc4dd1a2c
caleb1248 <caleb@tremontschool.org>
2024-06-09 12:50:31 -0400

    Update demo code

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,51 @@
-import './style.css';
-import './workers';
+import "./style.css";
+import "./workers";
 
-import * as monaco from 'monaco-editor';
-import { TokensProviderCache, convertTheme } from './textmate/index';
-import darkPlusTheme from './textmate/themes/dark-plus';
+import * as monaco from "monaco-editor";
+import { TokensProviderCache, convertTheme } from "./textmate/index";
+import darkPlusTheme from "./textmate/themes/dark-plus";
 
 // import eruda from 'eruda';
 // eruda.init();
 
-const editorDiv = document.createElement('div');
-editorDiv.classList.add('editor');
-document.getElementById('app')?.appendChild(editorDiv);
+const editorDiv = document.createElement("div");
+editorDiv.classList.add("editor");
+document.getElementById("app")?.appendChild(editorDiv);
 const model = monaco.editor.createModel(
-  'hii',
-  'typescript',
-  monaco.Uri.file('main.ts')
+  `// This is a demonstration of what textmate grammars can do, and what monaco grammars can't.
+
+let x = 5;
+const y = "hello world";
+
+interface IMyInterface {
+  foo: string;
+  bar: (baz: string) => number;
+}
+
+async function add(a: number, b: number) {
+  console.log('calculating \${a} + \${b}');
+  return a + b;
+}
+
+function subtract(a: number, b: number) {
+  console.log('calculating \${a} + \${b}');
+  return a + b;
+}
+
+export { add, add as default }`,
+  "typescript",
+  monaco.Uri.file("main.ts")
 );
 
 // Register textmate theme
 const theme = convertTheme(darkPlusTheme);
 
-monaco.editor.defineTheme('dark-plus', theme);
+monaco.editor.defineTheme("dark-plus", theme);
 
 const editor = monaco.editor.create(editorDiv, {
   model,
   tabSize: 2,
-  theme: 'dark-plus',
+  theme: "dark-plus",
 });
 
 // Begin textmate stuff
@@ -33,11 +53,11 @@ const editor = monaco.editor.create(editorDiv, {
 const cache = new TokensProviderCache(editor);
 
 monaco.languages.setTokensProvider(
-  'typescript',
-  await cache.getTokensProvider('source.ts')
+  "typescript",
+  await cache.getTokensProvider("source.ts")
 );
 
-window.addEventListener('resize', () => editor.layout());
+window.addEventListener("resize", () => editor.layout());
 
 // @ts-ignore
 window.editor = editor;
